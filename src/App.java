a08f95ccebc2f08121ddd6b3592e69799240ddf7
Stefan <stefan.holmberg@systementor.se>
2024-09-18 12:34:02 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //Övningen
+
+
+        System.out.println("Nu startar programmet");
+        Exercise1.execute();
+        System.out.println("Nu slutar programmet");
+        // String email = System.console().readLine();
+        // if(email.contains("@")){
+
+        // }
+
+        // int x = 12;
+        // String namn = "Stefan";
+        // namn = namn.replace("efa", "hej");
+        // System.out.println(namn);
+    }
+}
---
+++ src/Exercise1.java
@@ -0,0 +1,82 @@
+import java.util.concurrent.TimeUnit;
+
+public class Exercise1 {
+    public static void execute()  throws Exception{
+        //Övning
+        System.out.println("Hej");
+    }
+}
+//     while (true){
+//         System.out.println("1. Spela");
+//         System.out.println("2. Avsluta");
+//         System.out.print("Ange val:");
+//         int val = Integer.parseInt(System.console().readLine());
+//         if(val == 1){   
+//             System.out.println("*** Game starting ***");
+//             T//imeUnit.SECONDS.sleep(5);
+//             System.out.println("*** Game ending ***");
+//         }
+//         else if(val == 2){
+//             break;
+//         }
+//         else
+//             System.out.print("Ogiltigt val");
+//     }
+            
+//      System.out.println("Continue");
+
+
+
+//     // char exitChar=' ';    
+//     // while (exitChar != 'Y'){
+//     //     System.out.println("Do this forever");
+//     //     System.out.println("Or write Y to exit?");
+//     //     String exit = System.console().readLine();
+//     //     exitChar = exit.charAt(0);
+//     // }
+//     //  System.out.println("Continue");
+
+//     // char exitChar=' ';    
+//     // do{        
+//     //     System.out.println("Do this forever");
+//     //     System.out.println("Or write Y to exit?");
+//     //     String exit = System.console().readLine();
+//     //     exitChar = exit.charAt(0);
+//     // } while (exitChar != 'Y');
+
+//     //  System.out.println("Continue");
+
+//     // //
+//     // //year = year + 4 är SAMMA som year += 4 
+//     // for(int year = 1972; year <= 2024;year+=4){
+//     //     System.out.println(year);
+//     // }
+
+//     // for(int year = 2024; year>= 1972;year--){
+//     //     System.out.println(year);
+//     // }
+
+
+//     // int year = 1972;
+//     // while(year <= 2024){
+//     //     System.out.println(year);
+//     //     year++;
+//     // }
+
+//     //     int x = 12;
+//     //     if(x == 12){
+//     //         System.out.println("Nu körs exercise1");
+//     //     }
+//     //     x = 100;
+//     //     int antalVarvILoopen = 0;
+//     //     while(x < 15){
+//     //         x = 99;
+//     //         antalVarvILoopen = antalVarvILoopen + 1;
+//     //         System.out.println("Nu körs exercise1");
+//     //         //x = x + 1;
+//     //         x++;
+//     //     }
+//     //     System.out.println("Vi körde " + antalVarvILoopen + " varv");
+
+//     }    
+// }
